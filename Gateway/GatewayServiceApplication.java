8088eacf4bc86f256713fbc2f7b93bfcdd1d26e3
ChristianMCE <cmce@unach.mx>
2021-02-28 20:33:01 -0600

    Update GatewayServiceApplication.java

--- Gateway/GatewayServiceApplication.java
+++ Gateway/GatewayServiceApplication.java
@@ -13,3 +13,24 @@ public class GatewayServiceApplication {
 	}
 
 }
+
+
+/* ######################   Propuesta No. 2 para implementar el archivo de arranque de la Aplicación de Gateway     ######################  */
+
+package com.formacionbdi.springboot.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class SpringbootServicioZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioZuulServerApplication.class, args);
+	}
+
+}
